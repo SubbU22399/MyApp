9172a5b43b6bfd4d280a917009222da24fda9a88
unknown <subbu22399@gmail.com>
2024-11-22 17:13:42 +0530

    URL update

--- chat-app-frontend/src/services/socket.js
+++ chat-app-frontend/src/services/socket.js
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', { transports: ['websocket'] });
+const socket = io(process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000');
 export default socket;
